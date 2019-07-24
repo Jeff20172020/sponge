bb7892cb91b05a02f5fec36d4c4a3cf5f42fd061
tangzhiyuan <tangzhiyuan@grean.com.cn>
2019-07-24 17:16:00 +0800

    nothing

---
+++ app/src/main/java/com/dangbei/gonzalez/sample/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.dangbei.gonzalez.sample;
+
+import android.app.Application;
+
+/**
+ * Created by guoxiaodong on 2017/7/28
+ */
+public class SampleApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
