48e1621e8f1ad6cc1f7196a107f5282cb11edaa1
tangzhiyuan <tangzhiyuan@grean.com.cn>
2019-07-24 17:26:54 +0800

    feature:nothing

--- app/src/main/java/com/dangbei/gonzalez/sample/SampleApplication.java
+++ app/src/main/java/com/dangbei/gonzalez/sample/SampleApplication.java
@@ -3,7 +3,7 @@ package com.dangbei.gonzalez.sample;
 import android.app.Application;
 
 /**
- * Created by guoxiaodong on 2017/7/28
+ * Created by tzy on 2017/7/28
  */
 public class SampleApplication extends Application {
     @Override
